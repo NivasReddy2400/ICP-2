c0ebe530c456d2810e2c07883e34614cf2ba6b7b
NivasReddyP <65546797+NivasReddy2400@users.noreply.github.com>
2024-08-29 17:39:10 -0500

    RandomVector.py

--- RandomVector.py
+++ RandomVector.py
@@ -4,13 +4,13 @@ vector = np.random.uniform(1, 20, size=20)
 
 matrix = vector.reshape(4, 5)
 
+print("Original Matrix:")
+print(matrix)
+
 max_indices = np.argmax(matrix, axis=1)
 
 rows = np.arange(matrix.shape[0])
 matrix[rows, max_indices] = 0
-
-print("Original Matrix:")
-print(vector.reshape(4, 5))
 
 print("\nModified Matrix:")
 print(matrix)
